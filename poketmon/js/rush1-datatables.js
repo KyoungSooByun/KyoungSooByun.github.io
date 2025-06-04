9e61830b6efa39547f70d813b618a54aef8d3204
KyoungSooByun <163635119+KyoungSooByun@users.noreply.github.com>
2025-06-04 13:40:20 +0900

    Add files via upload

---
+++ poketmon/js/rush1-datatables.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+    // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+    let options1 = {
+        searchable: false,
+        sortable: false,
+        perPageSelect: false,
+        footer : false,
+        labels : {
+            info: ""
+        },
+        paging: false
+    };
+
+    let options2 = {
+        data: rush1Data,
+        searchable: false,
+        sortable: false,
+        perPageSelect: false,
+        footer : false,
+        labels : {
+            info: ""
+        },
+        paging: false
+    };
+
+    
+    const poketmonSelect = document.getElementById('poketmonSelect');
+    if (poketmonSelect) {
+        new simpleDatatables.DataTable(poketmonSelect, options1);
+    }
+
+    const datatablesSimple = document.getElementById('datatablesSimple');
+    if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple, options2);
+    }
+
+});
